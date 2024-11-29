36e75d07ba1b4d1a90692343d3972a6aa4b5caf7
Knut Magne Huglen <knut-magne.huglen@soprasteria.com>
2024-11-29 14:35:42 +0100

    Added support for NAMRON AS/4512773 - Namron Zigbee 8 Channel Switch Black Better naming of Buttons in Smarthings App Refactor button mappings and removed debug messages Removed support for Button Held (as it was not working) Removed unused code

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# smartthings_edge_drivers
+# SmartThings Edge Drivers
 
-## Smartthings Channel
+## Edge Driver Channel
 
-Name: kmhuglen smartthings edge drivers - dev
+| Channel Name | Channel ID | Description |
+|--------------|------------|-------------|
+| [kmhuglen smartthings edge drivers - dev](https://bestow-regional.api.smartthings.com/invite/kVlnyX0YzL24) | e17ea87b-a462-4671-856b-8bbfcd35abff | My personal drivers in development - Use at your own risk.|
 
-URL:  https://bestow-regional.api.smartthings.com/invite/kVlnyX0YzL24
-
-## Drivers
-- Namron AS Zigbee 8 channel switch white - https://www.zigbee2mqtt.io/devices/4512772.html
+## Devices
+- [Namron AS Zigbee 8 channel switch white 4512772](https://www.elektroimportoren.no/namron-zigbee-kanalbryter-hvit/4512772/Product.html)
+- [Namron AS Zigbee 8 channel switch black 4512773](https://www.elektroimportoren.no/namron-zigbee-kanalbryter-sort/4512773/Product.html)
--- zigbee/namron-4512772/src/init.lua
+++ zigbee/namron-4512772/src/init.lua
@@ -3,15 +3,33 @@ local ZigbeeDriver = require "st.zigbee"
 local defaults = require "st.zigbee.defaults"
 local device_management = require "st.zigbee.device_management"
 local clusters = require "st.zigbee.zcl.clusters"
-local cluster_base = require "st.zigbee.cluster_base"
-local data_types = require "st.zigbee.data_types"
 
-local comp = {"button1", "button2", "button3", "button4", "button5", "button6", "button7", "button8"}
+local comp = {
+  "buttonGroup1On",
+  "buttonGroup1Off",
+  "buttonGroup2On",
+  "buttonGroup2Off",
+  "buttonGroup3On",
+  "buttonGroup3Off",
+  "buttonGroup4On",
+  "buttonGroup4Off"
+}
+
+local button_map = {
+  ["01_01"] = 1,
+  ["01_00"] = 2,
+  ["02_01"] = 3,
+  ["02_00"] = 4,
+  ["03_01"] = 5,
+  ["03_00"] = 6,
+  ["04_01"] = 7,
+  ["04_00"] = 8
+}
 
 -- Define the button_handler to handle Zigbee messages for button presses
 local button_handler = function(driver, device, zb_rx)
-  -- Log the full Zigbee message for debugging
-  device.log.debug("Full Zigbee message: " .. tostring(zb_rx))
+  -- DEBUG Log the full Zigbee message for debugging
+  --device.log.debug("Full Zigbee message: " .. tostring(zb_rx))
 
   -- Convert the Zigbee message to a string
   local zb_rx_str = tostring(zb_rx)
@@ -20,80 +38,57 @@ local button_handler = function(driver, device, zb_rx)
   local src_endpoint = zb_rx_str:match("src_endpoint: 0x(%x+)")
   local zcl_command_id = zb_rx_str:match("ZCLCommandId: 0x(%x+)")
 
-  -- Log the extracted values
-  device.log.debug("Extracted src_endpoint: " .. (src_endpoint or "not found"))
-  device.log.debug("Extracted ZCLCommandId: " .. (zcl_command_id or "not found"))
+  local key = src_endpoint .. "_" .. zcl_command_id
+  local button_index = button_map[key]
 
-  local pushed_ev = capabilities.button.button.pushed()
-  pushed_ev.state_change = true
+  -- DEBUG Log the extracted values
+  --device.log.debug("src_endpoint (ButtonGroup number): " .. (src_endpoint or "not found"))
+  --device.log.debug("ZCLCommandId (01 = On, 00 = Off): " .. (zcl_command_id or "not found"))
 
-  if src_endpoint == "01" and zcl_command_id == "01" then
-    device.log.debug("Button1 pressed")
-    device.profile.components[comp[1]]:emit_event(pushed_ev)
-  elseif src_endpoint == "01" and zcl_command_id == "00" then
-    device.log.debug("Button2 pressed")
-    device.profile.components[comp[2]]:emit_event(pushed_ev)
-  elseif src_endpoint == "02" and zcl_command_id == "01" then
-    device.log.debug("Button3 pressed")
-    device.profile.components[comp[3]]:emit_event(pushed_ev)
-  elseif src_endpoint == "02" and zcl_command_id == "00" then
-    device.log.debug("Button4 pressed")
-    device.profile.components[comp[4]]:emit_event(pushed_ev)
-  elseif src_endpoint == "03" and zcl_command_id == "01" then
-    device.log.debug("Button5 pressed")
-    device.profile.components[comp[5]]:emit_event(pushed_ev)
-  elseif src_endpoint == "03" and zcl_command_id == "00" then
-    device.log.debug("Button6 pressed")
-    device.profile.components[comp[6]]:emit_event(pushed_ev)
-  elseif src_endpoint == "04" and zcl_command_id == "01" then
-    device.log.debug("Button7 pressed")
-    device.profile.components[comp[7]]:emit_event(pushed_ev)
-  elseif src_endpoint == "04" and zcl_command_id == "00" then
-    device.log.debug("Button8 pressed")
-    device.profile.components[comp[8]]:emit_event(pushed_ev)
+  if button_index then
+    local pushed_ev = capabilities.button.button.pushed()
+    pushed_ev.state_change = true
+    device.profile.components[comp[button_index]]:emit_event(pushed_ev)
   else
-    device.log.debug("Different button press detected.")
+    device.log.warn("Unmapped button press detected.")
   end
 end
 
 -- Function called when the device is added
 local device_added = function(driver, device)
-    device:emit_event(capabilities.button.supportedButtonValues({"pushed", "held"}))
-    device:emit_event(capabilities.button.button.pushed())
-    local n_button = 8 -- Assuming device has 8 buttons
-    for i = 1, n_button, 1 do
-        device.profile.components[comp[i]]:emit_event(capabilities.button.supportedButtonValues({"pushed", "held"}))
-        device.profile.components[comp[i]]:emit_event(capabilities.button.button.pushed())
-    end
+  local num_button = 8 -- Assuming device has 8 buttons
+  for i = 1, num_button, 1 do
+      local component_id = comp[i]
+      if device.profile.components[component_id] then
+          -- device.log.debug("Initializing button " .. i .. " (" .. component_id .. ")")
+          device.profile.components[component_id]:emit_event(capabilities.button.supportedButtonValues({"pushed"})) -- Assuming button only support pushed
+      else
+          device.log.warn("Component " .. component_id .. " does not exist in the device profile")
+      end
+  end
 end
 
 -- Configuration function for the device
 local do_configure = function(self, device)
     device:configure()
-    device:send(device_management.build_bind_request(device, 0xFC00, device.driver.environment_info.hub_zigbee_eui))
     device:send(clusters.PowerConfiguration.attributes.BatteryPercentageRemaining:read(device))
 end
 
 -- Define the driver with capabilities, zigbee handlers, and lifecycle handlers
 local namron_driver = {
   supported_capabilities = {
-      capabilities.button,
-      capabilities.battery,
+    capabilities.battery,
   },
   zigbee_handlers = {
-      cluster = {
-          [0xFC00] = {
-              [0x00] = button_handler
-          },
-          [clusters.OnOff.ID] = {
-              [clusters.OnOff.commands.On.ID] = button_handler,
-              [clusters.OnOff.commands.Off.ID] = button_handler
-          }
-      },
+    cluster = {
+        [clusters.OnOff.ID] = {
+            [clusters.OnOff.commands.On.ID] = button_handler,
+            [clusters.OnOff.commands.Off.ID] = button_handler
+        }
+    },
   },
   lifecycle_handlers = {
       added = device_added,
-      infoChanged = device_info_changed,
       doConfigure = do_configure
   }
 }
@@ -102,5 +97,5 @@ local namron_driver = {
 defaults.register_for_default_handlers(namron_driver, namron_driver.supported_capabilities)
 
 -- Run the Zigbee driver
-local zigbee_driver = ZigbeeDriver("namron-kanalbryter", namron_driver)
+local zigbee_driver = ZigbeeDriver("namron_driver", namron_driver)
 zigbee_driver:run()
